80a59d01d4f0a936917a2867d725be6da1c96eeb
Manoj Katuwal <mkc684867@gmail.com>
2026-08-22 18:05:04 +0545

    feat(routes): add routes for creating installment plans with authentication and authorization

--- backend/src/modules/installment/installmentPlan.controller.js
+++ backend/src/modules/installment/installmentPlan.controller.js
@@ -3,14 +3,12 @@ import { successResponse } from "../../shared/utils/response/apiResponse.js";
 import { createInstallmentPlanService } from "./installmentPlan.service.js";
 
 export const createInstallmentPlan = asyncHandler(async (req, res) => {
-      const plan = await createInstallmentPlanService(req.body, req.user.id);
+  const plan = await createInstallmentPlanService(req.body, req.user.id);
 
-      return successResponse({
-        res,
-        statusCode: 201,
-        message: "Installment plan सफलतापूर्वक सिर्जना भयो",
-        data : plan 
-      });
-
-
-})
+  return successResponse({
+    res,
+    statusCode: 201,
+    message: "Installment plan सफलतापूर्वक सिर्जना भयो",
+    data: plan,
+  });
+});
---
+++ backend/src/modules/installment/installmentPlan.routes.js
@@ -0,0 +1,21 @@
+import express from "express"
+import authenticate from "../../middleware/authenticate.js";
+import authorize from "../../middleware/authorize.js";
+import validate from "../../middleware/validate.js";
+import { createInstallmentPlan } from "./installmentPlan.repository.js";
+
+
+const router = express.Router();
+
+router.post(
+  "/",
+  authenticate,
+  authorize("ADMIN", "ACCOUNTANT"),
+  validate(createInstallmentPlanSchema),
+  createInstallmentPlan,
+);
+
+
+
+
+export default router;
