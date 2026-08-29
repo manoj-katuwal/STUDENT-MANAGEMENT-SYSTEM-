25b7bfce6c8a99287dfbda6230609979c9e7604a
Manoj Katuwal <mkc684867@gmail.com>
2026-08-29 15:10:13 +0545

    feat: add axios client and token utility functions for API authentication

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials : true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default apiClient;
