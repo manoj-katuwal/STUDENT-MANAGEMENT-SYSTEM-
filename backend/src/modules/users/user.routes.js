c61bc986179a37dbf57bfaf189761a45ef87b7b5
Manoj Katuwal <mkc684867@gmail.com>
2026-09-02 11:18:07 +0545

    feat: add authentication middleware to user routes for enhanced security

--- backend/src/modules/users/user.routes.js
+++ backend/src/modules/users/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import authenticate from "../../middleware/authenticate.js";
 import authorize from "../../middleware/authorize.js";
 import validate from "../../middleware/validate.js";
 import {
@@ -19,19 +20,35 @@ import { registerUserSchema } from "./user.validation.js";
 const router = Router();
 
 router.post("/register", validate(registerUserSchema), registerUserController);
-router.get("/me", getMyProfileController);
-router.patch("/me", updateMyProfileController);
+router.get("/me", authenticate, getMyProfileController);
+router.patch("/me", authenticate, updateMyProfileController);
 
-router.post("/", authorize("ADMIN"), createUserController);
-router.get("/", authorize("ADMIN"), getAllUsersController);
-router.get("/:userId", authorize("ADMIN"), getUserByIdController);
-router.patch("/:userId/role", authorize("ADMIN"), changeUserRoleController);
-router.patch("/:userId/activate", authorize("ADMIN"), activateUserController);
+router.post("/", authenticate, authorize("ADMIN"), createUserController);
+router.get("/", authenticate, authorize("ADMIN"), getAllUsersController);
+router.get("/:userId", authenticate, authorize("ADMIN"), getUserByIdController);
+router.patch(
+  "/:userId/role",
+  authenticate,
+  authorize("ADMIN"),
+  changeUserRoleController,
+);
+router.patch(
+  "/:userId/activate",
+  authenticate,
+  authorize("ADMIN"),
+  activateUserController,
+);
 router.patch(
   "/:userId/deactivate",
+  authenticate,
   authorize("ADMIN"),
   deactivateUserController,
 );
-router.delete("/:userId", authorize("ADMIN"), deleteUserController);
+router.delete(
+  "/:userId",
+  authenticate,
+  authorize("ADMIN"),
+  deleteUserController,
+);
 
 export default router;
